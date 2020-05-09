9570ced884fd7b783de5a366478ffdeaa560da31
Nahuel Cirocco <nahuel.cirocco@gmail.com>
2020-05-09 11:39:22 +0200

    User regex to get serial code

--- serial.go
+++ serial.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
+	"regexp"
 )
 
-const cdromStr string = "cdrom:\\"
+const serialRegex string = "(SLPS|SLES|SLUS|SCPS|SCUS|SCES|SIPS|SLPM|SLEH|SLED|SCED|ESPM|PBPX)_\\d{3}.\\d{2}"
 const serialCodeLength int = 11
 
 // GetSerial returns the serial for the given PSX bin file
 func GetSerial(filepath string) (string, error) {
-	const BufferSize = 100
+	const BufferSize = 1024 * 1024
 	serial := ""
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -21,6 +21,7 @@ func GetSerial(filepath string) (string, error) {
 	defer file.Close()
 
 	buffer := make([]byte, BufferSize)
+	m := regexp.MustCompile(serialRegex)
 	for {
 		bytesread, err := file.Read(buffer)
 		if err != nil {
@@ -31,9 +32,9 @@ func GetSerial(filepath string) (string, error) {
 			break
 		}
 
-		position := strings.Index(string(buffer[:bytesread]), cdromStr)
-		if position >= 0 {
-			serial = string(buffer[:bytesread])[position+len(cdromStr) : position+len(cdromStr)+serialCodeLength]
+		positions := m.FindStringIndex(string(buffer[:bytesread]))
+		if len(positions) > 0 {
+			serial = string(buffer[:bytesread])[positions[0] : positions[0]+serialCodeLength]
 
 			break
 		}
