6563111e5e5a799b1976664734b1e4ef572c2362
Nahuel Cirocco <nahuel.cirocco@gmail.com>
2020-05-12 13:32:59 +0200

    serial.go

--- serial.go
+++ serial.go
@@ -5,10 +5,18 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"strings"
 )
 
-const serialRegex string = "(SLPS|SLES|SLUS|SCPS|SCUS|SCES|SIPS|SLPM|SLEH|SLED|SCED|ESPM|PBPX)_\\d{3}.\\d{2}"
+const serialRegex string = "((SLPS|SLES|SLUS|SCPS|SCUS|SCES|SIPS|SLPM|SLEH|SLED|SCED|ESPM|PBPX|LSP)[_P\\-])|(LSP9|907127)"
+const serialCodeDotPosition int = 8
 const serialCodeLength int = 11
+
+var serialExceptions = map[string]string{
+	"SLUSP":  "SLUS_",
+	"LSP9":   "LSP_9",
+	"907127": "LSP_907127",
+}
 
 // GetSerial returns the serial for the given PSX bin file
 func GetSerial(filepath string) (string, error) {
@@ -35,6 +43,17 @@ func GetSerial(filepath string) (string, error) {
 		positions := m.FindStringIndex(string(buffer[:bytesread]))
 		if len(positions) > 0 {
 			serial = string(buffer[:bytesread])[positions[0] : positions[0]+serialCodeLength]
+			serial = strings.Replace(serial, ".", "", 1)
+			serial = strings.Replace(serial, "-", "_", 1)
+			serial = strings.Replace(serial, "-", "", 1)
+
+			for key, value := range serialExceptions {
+				if strings.Contains(serial, key) {
+					serial = strings.Replace(serial, key, value, 1)
+				}
+			}
+
+			serial = serial[:serialCodeDotPosition] + "." + serial[serialCodeDotPosition:serialCodeLength-1]
 
 			break
 		}
