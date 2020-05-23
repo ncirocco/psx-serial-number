afb92cbbb240ee41216d9fa7e3eda97dc6f72c3b
Nahuel Cirocco <nahuel.cirocco@gmail.com>
2020-05-23 14:39:35 +0200

    Add tests

--- serial.go
+++ serial.go
@@ -11,6 +11,7 @@ import (
 const serialRegex string = "((SLPS|SLES|SLUS|SCPS|SCUS|SCES|SIPS|SLPM|SLEH|SLED|SCED|ESPM|PBPX|LSP)[_P\\-])|(LSP9|907127)"
 const serialCodeDotPosition int = 8
 const serialCodeLength int = 11
+const bufferSize int = 1024 * 1024
 
 var serialExceptions = map[string]string{
 	"SLUSP":  "SLUS_",
@@ -18,18 +19,18 @@ var serialExceptions = map[string]string{
 	"907127": "LSP_907127",
 }
 
+var m = regexp.MustCompile(serialRegex)
+
 // GetSerial returns the serial for the given PSX bin file
-func GetSerial(filepath string) (string, error) {
-	const BufferSize = 1024 * 1024
-	serial := ""
+func GetSerial(filepath string) (serial string, err error) {
 	file, err := os.Open(filepath)
 	if err != nil {
 		return serial, err
 	}
 	defer file.Close()
 
-	buffer := make([]byte, BufferSize)
-	m := regexp.MustCompile(serialRegex)
+	buffer := make([]byte, bufferSize)
+
 	for {
 		bytesread, err := file.Read(buffer)
 		if err != nil {
@@ -40,21 +41,10 @@ func GetSerial(filepath string) (string, error) {
 			break
 		}
 
-		positions := m.FindStringIndex(string(buffer[:bytesread]))
-		if len(positions) > 0 {
-			serial = string(buffer[:bytesread])[positions[0] : positions[0]+serialCodeLength]
-			serial = strings.Replace(serial, ".", "", 1)
-			serial = strings.Replace(serial, "-", "_", 1)
-			serial = strings.Replace(serial, "-", "", 1)
-
-			for key, value := range serialExceptions {
-				if strings.Contains(serial, key) {
-					serial = strings.Replace(serial, key, value, 1)
-				}
-			}
-
-			serial = serial[:serialCodeDotPosition] + "." + serial[serialCodeDotPosition:serialCodeLength-1]
-
+		serial = findSerial(string(buffer[:bytesread]))
+		if serial != "" {
+			fmt.Println(serial)
+			serial = normalizeSerial(serial)
 			break
 		}
 	}
@@ -64,4 +54,27 @@ func GetSerial(filepath string) (string, error) {
 	}
 
 	return serial, nil
+}
+
+func findSerial(s string) string {
+	serialPosition := m.FindStringIndex(s)
+	if len(serialPosition) > 0 {
+		return s[serialPosition[0] : serialPosition[0]+serialCodeLength]
+	}
+
+	return ""
+}
+
+func normalizeSerial(s string) string {
+	serial := strings.Replace(s, ".", "", 1)
+	serial = strings.Replace(serial, "-", "_", 1)
+	serial = strings.Replace(serial, "-", "", 1)
+
+	for key, value := range serialExceptions {
+		if strings.Contains(serial, key) {
+			serial = strings.Replace(serial, key, value, 1)
+		}
+	}
+
+	return serial[:serialCodeDotPosition] + "." + serial[serialCodeDotPosition:serialCodeLength-1]
 }
